3d39a562f5604fc35f5122ef925052364a5526ad
YiFei Zhu <zhuyifei1999@gmail.com>
2022-08-30 01:42:37 -0700

    08_hook_challenge: Add some explanation for additional env vars
    Mostly what and why they are needed here.

--- 08_hook_challenge/run.sh
+++ 08_hook_challenge/run.sh
@@ -8,7 +8,14 @@ if [[ ! -z "$EXPORT_PATH" && ! -f "$EXPORT_PATH"/db/flags.json ]]; then
   EXPORT_PATH=
 fi
 
-[[ -z "$CHAL_REPO" && -z "$EXPORT_PATH" ]] && die '$CHAL_REPO and $EXPORT_PATH both not set'
+if [[ -z "$CHAL_REPO" && -z "$EXPORT_PATH" ]]; then
+  echo 'Error: $CHAL_REPO and $EXPORT_PATH both not set'
+  echo 'This stage requires chal flags so that hashes of the flags may be added to client side JS so that the flag checking may be done on the client side.'
+  echo '- Set $CHAL_REPO if you want the script to obtain the flags from the ctfcli challenge.yml files (will read from ${CHAL_REPO}/**/challenge.yml).'
+  echo '- Set $EXPORT_PATH if you want the script to obtain the flags from CTFd export (will read from ${EXPORT_PATH}/db/flags.json).'
+  echo 'If you have both, $EXPORT_PATH is preferred :)'
+  die
+fi
 
 export CHAL_REPO EXPORT_PATH
 if [[ ! -z "$CHAL_REPO" ]]; then
