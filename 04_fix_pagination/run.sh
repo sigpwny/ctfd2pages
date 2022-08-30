3a61c5d86f33be64020c8917117d73cc775c39a3
YiFei Zhu <zhuyifei1999@gmail.com>
2022-08-30 01:11:40 -0700

    04_fix_pagination: Fix commit message

--- 04_fix_pagination/run.sh
+++ 04_fix_pagination/run.sh
@@ -3,4 +3,4 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$DIR"/../lib.sh
 
-do_stage commit_command 'Remove login & register pages so as not to confuse people'
+do_stage commit_command 'Make teams & users pagination work statically'
